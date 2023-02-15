a5831e45b0d70323368b5f66c8a01ff4584c0cbd
CS_DEV <christian.sadikmelik@gmail.com>
2023-02-15 18:14:56 +0100

    edit language

--- libraryProject/libraryProject/settings.py
+++ libraryProject/libraryProject/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'libraryProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'library-crm',
+       'USER': 'postgres',
+       'PASSWORD': 'root',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
 
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it-it'
 
 TIME_ZONE = 'UTC'
 

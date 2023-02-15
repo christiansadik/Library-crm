84315b9d320a55ca4d0eb3f5922babf6de084dc7
CS_DEV <christian.sadikmelik@gmail.com>
2023-02-15 19:41:33 +0100

    add url for homepage

--- libraryProject/libraryProject/urls.py
+++ libraryProject/libraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import homepage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage'),
     path('library/', include('library.urls'))
 ]

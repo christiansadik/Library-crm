5b7602c29242a39dfa0fd3916937ccaaebaab844
CS_DEV <christian.sadikmelik@gmail.com>
2023-02-15 19:20:32 +0100

    add function author

--- libraryProject/library/views.py
+++ libraryProject/library/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Author
 # Create your views here.
+
+def author(request, id):
+    author = get_object_or_404(Author, id=id)
+    content = {'author':author}
+    return render (request, 'author.html', content)

3744b6ada86f9c15227748f52094648d1414fc6a
CS_DEV <christian.sadikmelik@gmail.com>
2023-02-16 11:41:20 +0100

    edit views, urls and template with Detail View Class

--- libraryProject/library/views.py
+++ libraryProject/library/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Author
 # Create your views here.
 
+from django.views.generic.detail import DetailView
 
-def author(request, id):
-    author = get_object_or_404(Author, id=id)
-    content = {'author':author}
-    return render (request, 'author.html', content)
+# With function
+# def author(request, id):
+#     author = get_object_or_404(Author, id=id)
+#     content = {'author':author}
+#     return render (request, 'author.html', content)
+
+# With Detail List View Class
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'author.html'
